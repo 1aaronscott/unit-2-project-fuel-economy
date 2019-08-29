98984231bafafd9f6ce5c09ff08b1eb985e5a87b
damack <damack@beast.beast>
2019-08-29 15:23:22 -0500

    Committer: 2019089-2  Changes to be committed: 	modified:   pages/index.py 	modified:   pages/process.py

--- pages/index.py
+++ pages/index.py
@@ -31,8 +31,8 @@ column1 = dbc.Col(
         
             ## Value Proposition
 
-            Peak Oil. Remeber that? No one talks about that anymore and with the state of the world we may never even get there. But just in case we do, this web app will attempt to model fuel economy based on published, historical
-            data from the Evironmental Protection Agency. Since they are still by far the most common, the focus is on conventional fuel-type vehicles.
+            Peak Oil. Remember that? No one talks about that anymore and with the state of the world we may never even get there. But just in case we do, this web app will attempt to model fuel economy based on published, historical
+            data from the Environmental Protection Agency. Since they are still by far the most common, the focus is on conventional fuel-type vehicles.
 
             """
         ),
